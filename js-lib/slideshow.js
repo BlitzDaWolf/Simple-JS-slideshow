3878301a845571faa82e1d961707d937fa0ee07f
BlitzDaWolf <botjes2@hotmail.com>
2017-03-07 21:54:07 +0100

    Update slideshow.js

--- js-lib/slideshow.js
+++ js-lib/slideshow.js
@@ -2,6 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) Tue Mar 07 2017 BlitzDaWolf
+Github: https://github.com/BlitzDaWolf/Simple-JS-slideshow
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
